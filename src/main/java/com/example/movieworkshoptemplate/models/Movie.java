1b8a2967356a20bd276d7d7a75ec55b09143645d
Simon Roswall <simonroswall@gmail.com>
2021-09-27 09:53:48 +0200

    create Movie class in model

--- src/main/java/com/example/movieworkshoptemplate/models/Movie.java
+++ src/main/java/com/example/movieworkshoptemplate/models/Movie.java
@@ -2,4 +2,69 @@ package com.example.movieworkshoptemplate.models;
 
 //Movie POJO
 public class Movie {
+    private int year;
+    private int length;
+    private String title;
+    private String subject;
+    private int popularity;
+    private boolean awards;
+
+    public Movie(){};
+
+    public Movie(int year, int length, String title, String subject, int popularity, boolean awards) {
+        this.year = year;
+        this.length = length;
+        this.title = title;
+        this.subject = subject;
+        this.popularity = popularity;
+        this.awards = awards;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(int popularity) {
+        this.popularity = popularity;
+    }
+
+    public boolean getAwards() {
+        return awards;
+    }
+
+    public void setAwards(boolean awards) {
+        this.awards = awards;
+    }
 }
