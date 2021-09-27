c649787e91b1a0417d6f9ff3e673705054c4f47e
Simon Roswall <simonroswall@gmail.com>
2021-09-27 09:47:54 +0200

    initial commit add temp endpoints

--- src/main/java/com/example/movieworkshoptemplate/controllers/MovieController.java
+++ src/main/java/com/example/movieworkshoptemplate/controllers/MovieController.java
@@ -1,6 +1,7 @@
 package com.example.movieworkshoptemplate.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,30 @@ public class MovieController {
     public String index(){
         return "Hello World welcome to my application";
     }
+
+    @GetMapping("/getFirst")
+    public String getFirst(){
+        return "temp";
+    }
+
+    @GetMapping("/getRandom")
+    public String getRandom(){
+        return "temp";
+    }
+
+    @GetMapping("/getTenSortByPopularity")
+    public String getTenSortByPopularity(){
+        return "temp";
+    }
+
+    @GetMapping("/howManyWonAnAward")
+    public String howManyWonAnAward(){
+        return "temp";
+    }
+
+    @GetMapping("/filter")
+    public String filterBy(@RequestParam char letter, int amount){
+        return "temp";
+    }
+
 }
