b6bf47b06ea7c2509e63297c83c741258c930bc1
Simon Roswall <simonroswall@gmail.com>
2021-09-30 00:09:45 +0200

    add HTML formatting to /

--- src/main/java/com/example/movieworkshop/controllers/MovieController.java
+++ src/main/java/com/example/movieworkshop/controllers/MovieController.java
@@ -12,7 +12,17 @@ public class MovieController {
     //Controller methods
     @GetMapping("/")
     public String index(){
-        return "Hello World welcome to my application";
+
+        String url = "<br><a href=/";
+        String getFirst = url+"getFirst>Get the first movie</a>";
+        String getRandom =  url + "getRandom>Get a random movie</a>";
+        String getTen = url + "getTenSortByPopularity>Get the ten most popular</a>";
+        String howMany = url + "howManyWonAnAward>See how many movies won an award</a>";
+        String filter = url + "filter>Filter movies by letter and amount</a>";
+        String longest = url + "longest>Compare average runtime of genres</a>";
+
+        return "<h1> Hello World welcome to my application </h1>"+ getFirst + getRandom + getTen +
+                howMany + filter + longest;
     }
 
     @GetMapping("/getFirst")
